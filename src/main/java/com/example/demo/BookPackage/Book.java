020d45b6825ebf729a2924e814bd10d36fb6022b
artemkurilov-backend <artemgithub101@gmail.com>
2026-04-25 14:00:39 -0700

    Add files via upload

---
+++ src/main/java/com/example/demo/BookPackage/Book.java
@@ -0,0 +1,90 @@
+package com.example.demo.BookPackage;
+
+import com.example.demo.BorrowPackage.Borrow;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String author;
+    private String title;
+    private int quantity;
+    @Lob
+    private String content;
+
+
+    @OneToMany(mappedBy = "book")
+    private List<Borrow> borrows;
+
+
+    public Book() {
+    }
+
+    public Book(long id) {
+        this.id = id;
+    }
+
+    public Book(long id, String author, String title, int quantity, String content, List<Borrow> borrows) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.quantity = quantity;
+        this.content = content;
+        this.borrows = borrows;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Borrow> getBorrows() {
+        return borrows;
+    }
+
+    public void setBorrows(List<Borrow> borrows) {
+        this.borrows = borrows;
+    }
+}
---
+++ src/main/java/com/example/demo/BookPackage/BookController.java
@@ -0,0 +1,57 @@
+package com.example.demo.BookPackage;
+
+import com.example.demo.BookPackage.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/BookController")
+public class BookController {
+
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/ListOfBooks")
+    public String listOfBooks(Model model){
+        model.addAttribute("books", service.getAllBooks());
+        return "getAllBooks";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        service.deleteBook(id);
+        return "redirect:/BookController/ListOfBooks";
+    }
+    @GetMapping("/formAddBook")
+    public String openFormAddStudent(Model model){
+        model.addAttribute("book", new Book());
+        return "formBook";
+    }
+    @PostMapping("/formAddBook/new")
+    public String createBook(@ModelAttribute Book book){
+        service.saveBook(book);
+        return "redirect:/BookController/ListOfBooks";
+    }
+    @GetMapping("/open/{id}")
+    public String openBook(@PathVariable Long id, Model model){
+        Book book = service.openBook(id);
+        model.addAttribute("book", book);
+        return "book";
+    }
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, Model model){
+        Book book = service.editBook(id);
+        model.addAttribute("book", book);
+        return "editBook";
+    }
+
+    @PostMapping("/update")
+    public String updateBook(@ModelAttribute Book book){
+        service.saveBook(book);
+        return "redirect:/BookController/ListOfBooks";
+    }
+
+}
