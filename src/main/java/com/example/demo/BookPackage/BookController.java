ad63e06a18c94cf51b20d8f1d27b83b541af96c2
artemkurilov-backend <artemgithub101@gmail.com>
2026-05-06 17:44:13 -0700

    Add files via upload

---
+++ src/main/java/com/example/demo/BookPackage/BookController.java
@@ -0,0 +1,92 @@
+package com.example.demo.BookPackage;
+
+import com.example.demo.BookPackage.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/BookController")
+public class BookController {
+
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/ListOfBooks")
+    public String listOfBooks(Model model,
+                              @RequestParam(defaultValue = "0") int page){
+
+        int pageSize = 20;
+
+        Page<Book> bookPage = service.getAllBooks(page, pageSize);
+
+        model.addAttribute("books", bookPage.getContent());
+
+        model.addAttribute("currentPage", page);
+
+        model.addAttribute("totalPages", bookPage.getTotalPages());
+
+        return "getAllBooks";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        service.deleteBook(id);
+        return "redirect:/BookController/ListOfBooks";
+    }
+    @GetMapping("/formAddBook")
+    public String openFormAddStudent(Model model){
+        model.addAttribute("book", new Book());
+        return "formBook";
+    }
+    @PostMapping("/formAddBook/new")
+    public String createBook(@ModelAttribute Book book){
+        String quantity = book.getQuantity();
+        if(!quantity.matches("\\d+")){
+            return"errorPageBOOK";
+        }
+
+        service.saveBook(book);
+        return "redirect:/BookController/ListOfBooks";
+    }
+    @GetMapping("/open/{id}")
+    public String openBook(@PathVariable Long id, Model model){
+        Book book = service.openBook(id);
+        model.addAttribute("book", book);
+        return "book";
+    }
+    @PostMapping("/edit/{id}")
+    public String updateBook(@PathVariable Long id,
+                             @RequestParam String author,
+                             @RequestParam String title,
+                             @RequestParam String quantity,
+                             @RequestParam String content){
+
+        Book book = service.openBook(id);
+
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setQuantity(quantity);
+        book.setContent(content);
+
+        service.saveBook(book);
+
+        return "redirect:/BookController/ListOfBooks";
+    }
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, Model model){
+        Book book = service.openBook(id);
+        model.addAttribute("book", book);
+        return "editBook";
+    }
+
+    @PostMapping("/update")
+    public String updateBook(@ModelAttribute Book book){
+        service.saveBook(book);
+        return "redirect:/BookController/ListOfBooks";
+    }
+
+}
